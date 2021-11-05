f092c13dc5689e817a916c6d87f2ecc4044cbb7d
ZikangLuo <brianlo8991@gmail.com>
2021-11-05 11:53:17 +0800

    map zoom in added

--- Workspace/app/src/main/java/com/example/myapplication/MapsActivity.java
+++ Workspace/app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -96,6 +97,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LatLng position = new LatLng(Double.parseDouble(Data.get(Data.size()-1).getLat()), Double.parseDouble(Data.get(Data.size()-1).getLng()));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(position)      // Sets the center of the map to Mountain View
+                .zoom(13)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
 
     }
